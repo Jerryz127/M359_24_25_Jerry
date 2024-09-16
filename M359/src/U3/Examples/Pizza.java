6ccd24e747ec2a2702a814b754f2bdea780b9156
Jerryz127 <zhang1363@students.d211.org>
2024-09-16 14:22:08 -0500

    made changes to pizza class

--- M359/src/U3/Examples/Pizza.java
+++ M359/src/U3/Examples/Pizza.java
@@ -11,6 +11,52 @@ public class Pizza {
         this.hasCheese = hasCheese;
     }
 
+    /*
+    The equals method MUST return a boolean and take
+    ONE parameter of the same type as the class (Pizza)
+    Always called equals
+    Ex: public boolean equals(Pizza other)
+     */
+
+    public boolean equals(Pizza other){
+        if(this.getToppings().equals(other.toppings) && this.diameter == other.diameter && this. hasCheese == other.hasCheese){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /*
+    If you print a pizza object WITHOUT having a defined toString
+    Method, Java will only print out the address in memory of the
+    reference variable(Ex.U3.Examples.Pizza@65ab7765)
+
+    create a method with signature:
+        public String toString(){ }
+
+    If a toString is defined in Pizza class, then it will be called
+    on your behalf whenever you print a Pizza reference variable
+     */
+
+    public String toString(){
+        String str = "";
+        str = "My " + diameter + "in pizza has " +toppings + " toppings";
+        if (!hasCheese){
+            str += " and no cheese";
+        }
+        return str;
+    }
+//
+//    @Override
+//    public String toString() {
+//        return "Pizza{" +
+//                "toppings='" + toppings + '\'' +
+//                ", diameter=" + diameter +
+//                ", hasCheese=" + hasCheese +
+//                '}';
+//    }
+
     public String getToppings() {
         return toppings;
     }
---
+++ M359/src/U3/Examples/PizzaTester.java
@@ -0,0 +1,12 @@
+package U3.Examples;
+
+public class PizzaTester {
+    public static void main(String[] args) {
+         Pizza a= new Pizza(14,"pepperoni",false);
+         Pizza b = new Pizza(12,"chicken",true);
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
