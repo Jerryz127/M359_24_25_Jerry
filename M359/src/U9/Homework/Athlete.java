d7ebf03384c73aa7fbc151ddcf2e4f1a72b7a0ad
Jerryz127 <zhang1363@students.d211.org>
2025-02-24 14:31:39 -0600

    Ws 1 U9 superclass and subclass

---
+++ M359/src/U9/Homework/Athlete.java
@@ -0,0 +1,27 @@
+package U9.Homework;
+
+public class Athlete {
+    private String name;
+    private int age;
+
+    public Athlete(String n, int a){
+        name = n;
+        age = a;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setName(String s){
+        name = s;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
+}
---
+++ M359/src/U9/Homework/TeamAthlete.java
@@ -0,0 +1,28 @@
+package U9.Homework;
+
+public class TeamAthlete extends Athlete{
+    private String teamName;
+    private String sport;
+
+    public TeamAthlete(String n, int a, String t, String s){
+        super(n, a);
+        teamName = t;
+        sport = s;
+    }
+
+    public String getTeamName(){
+        return teamName;
+    }
+
+    public String getSport(){
+        return sport;
+    }
+
+    public void setTeamName(String s){
+        teamName = s;
+    }
+
+    public void setSport(String a){
+        sport = a;
+    }
+}
